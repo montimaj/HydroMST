919a942af3a27e6f4671881f0315646ce87b06ba
montimaj <smxnv@mst.edu>
2019-08-20 11:44:29 -0500

    Added various raster operations and driver code

--- Python_Port/gw_pumping_rf.py
+++ Python_Port/gw_pumping_rf.py
@@ -1,5 +1,44 @@
 # Author: Sayantan Majumdar
 # Email: smxnv@mst.edu
 
+from Python_Port import rasterops as rops
+import numpy as np
+NO_DATA_VALUE = 255
 
+input_dir = '/Users/smxnv/Documents/Data/'
+output_dir = '/Users/smxnv/Documents/Output/'
+az_raster_path = input_dir + 'cropscape/CDL_2015_clip_20190812153756_568423369/' \
+                             'CDL_2015_clip_20190812153756_568423369.tif'
+az_ws_path = input_dir + 'watersheds/az_merged/az_watershed.shp'
+az_crop_file = output_dir + 'az_crop.tif'
+az_cropped = rops.crop_raster(az_raster_path, az_ws_path, az_crop_file)
 
+az_class_dict = {(0, 59.5): 1,
+                 (67.5, 75.5): 1,
+                 (203.5, 250): 1,
+                 (110.5, 111.5): 2,
+                 (120.5, 124.5): 3,
+                 (60.5, 61.5): NO_DATA_VALUE,
+                 (130.5, 195.5): NO_DATA_VALUE
+                 }
+az_reclass_file = output_dir + 'az_reclass.tif'
+az_reclass = rops.reclassify_raster(az_crop_file, az_class_dict, az_reclass_file)
+
+ks_raster_path = input_dir + 'cropscape/polygonclip_20190306140312_392696635/' \
+                             'CDL_2015_clip_20190306140312_392696635.tif'
+ks_ws_path = input_dir + 'watersheds/ks_merged/ks_watershed.shp'
+ks_crop_file = output_dir + 'ks_crop.tif'
+ks_cropped = rops.crop_raster(ks_raster_path, ks_ws_path, ks_crop_file)
+
+ks_class_dict = {(0, 59.5): 1,
+                 (66.5, 77.5): 1,
+                 (203.5, 255): 1,
+                 (110.5, 111.5): 2,
+                 (111.5, 112.5): NO_DATA_VALUE,
+                 (120.5, 124.5): 3,
+                 (59.5, 61.5): NO_DATA_VALUE,
+                 (130.5, 195.5): NO_DATA_VALUE
+                 }
+
+ks_reclass_file = output_dir + 'ks_reclass.tif'
+ks_reclass = rops.reclassify_raster(ks_crop_file, ks_class_dict, ks_reclass_file)
--- Python_Port/rasterops.py
+++ Python_Port/rasterops.py
@@ -2,78 +2,83 @@
 # Email: smxnv@mst.edu
 
 import numpy as np
-import os
 import matplotlib.pyplot as plt
 import rasterio as rio
 from rasterio.plot import plotting_extent
 from rasterio.mask import mask
 from shapely.geometry import mapping
 import geopandas as gpd
-import seaborn as sns
-# import gdal
-#plt.ion()
-
-# def write_file(arr, src_file, outfile='test', no_data_value=-32768, is_complex=True, type='rasterio'):
-#     """
-#     Write image files in TIF format
-#     :param arr: Image array to write
-#     :param src_file: Original image file for retrieving affine transformation parameters
-#     :param outfile: Output file path
-#     :param no_data_value: No data value to be set
-#     :param is_complex: If true, write complex image array in two separate bands
-#     :return: None
-#     """
-#
-#     driver = gdal.GetDriverByName("GTiff")
-#     if is_complex:
-#         out = driver.Create(outfile + ".tif", arr.shape[1], arr.shape[0], 2, gdal.GDT_Float32)
-#     else:
-#         out = driver.Create(outfile + ".tif", arr.shape[1], arr.shape[0], 1, gdal.GDT_Float32)
-#     proj = src_file.crs
-#     transform = src_file.transform
-#     if type == 'gdal':
-#         proj = src_file.GetProjection()
-#         transform = src_file.GetGeoTransform()
-#     out.SetProjection(proj)
-#     out.SetGeoTransform(transform)
-#     out.GetRasterBand(1).SetNoDataValue(no_data_value)
-#     if is_complex:
-#         arr[np.isnan(arr)] = no_data_value + no_data_value * 1j
-#         out.GetRasterBand(2).SetNoDataValue(no_data_value)
-#         out.GetRasterBand(1).WriteArray(arr.real)
-#         out.GetRasterBand(2).WriteArray(arr.imag)
-#     else:
-#         arr[np.isnan(arr)] = no_data_value
-#         out.GetRasterBand(1).WriteArray(arr)
-#     out.FlushCache()
+import numpy as np
+# import seaborn as sns
+# plt.ion()
 
 
-sns.set(font_scale=1.5)
-os.chdir('/Users/smxnv/Documents/Data')
-az_watershed = gpd.read_file('watersheds/az_merged/az_watershed.shp')
-az_watershed = mapping(az_watershed['geometry'][0])
-az_data = rio.open('cropscape/CDL_2015_clip_20190812153756_568423369/CDL_2015_clip_20190812153756_568423369.tif')
-az_crop, az_affine = mask(az_data, [az_watershed], crop=True)
-az_extent = plotting_extent(az_crop[0], az_affine)
+def write_raster(raster_data, raster_file, outfile_path, bands=1):
+    """
+    Write raster file in GeoTIFF format
+    :param raster_data: Raster data to be written
+    :param raster_file: Original raster file containing geo-coordinates
+    :param outfile_path: Outfile file path
+    :param bands: Number of bands to write
+    :return: None
+    """
 
-# fig, ax = plt.subplots(figsize=(10, 8))
-# az_plot = ax.imshow(az_crop[0], extent=az_extent)
-# ax.set_title("Cropped Raster Dataset")
-# ax.set_axis_off()
-# fig.colorbar(az_plot)
-# plt.show()
-az_crop = np.squeeze(az_crop)
-print(az_crop.shape)
-print(np.max(az_crop))
-# with rio.open(
-#     '../Output/AZ_Crop.tif',
-#     'w',
-#     driver='GTiff',
-#     height=az_crop.shape[0],
-#     width=az_crop.shape[1],
-#     dtype=az_crop.dtype,
-#     crs=az_data.crs,
-#     count=1,
-#     transform=az_data.transform,
-# ) as dst:
-#     dst.write(az_crop, 1)
+    with rio.open(
+            outfile_path,
+            'w',
+            driver='GTiff',
+            height=raster_data.shape[0],
+            width=raster_data.shape[1],
+            dtype=raster_data.dtype,
+            crs=raster_file.crs,
+            transform=raster_file.transform,
+            count=bands,
+            nodata=255
+    ) as dst:
+        dst.write(raster_data, bands)
+
+
+def crop_raster(input_raster_file, input_mask_path, outfile_path, plot_fig=False, plot_title=""):
+    """
+    Crop raster data based on given shapefile
+    :param input_raster_file: Input raster dataset path
+    :param input_mask_path: Shapefile path
+    :param outfile_path: Output file path (only tiff file)
+    :param plot_fig: If true, then cropped raster data is plotted
+    :param plot_title: Plot title to display
+    :return: Cropped raster dataset
+    """
+
+    shape_file = gpd.read_file(input_mask_path)
+    shape_file_geom = mapping(shape_file['geometry'][0])
+    raster_data = rio.open(input_raster_file)
+    raster_crop, raster_affine = mask(raster_data, [shape_file_geom])
+    shape_extent = plotting_extent(raster_crop[0], raster_affine)
+    raster_crop = np.squeeze(raster_crop)
+    write_raster(raster_crop, raster_data, outfile_path=outfile_path)
+    if plot_fig:
+        fig, ax = plt.subplots(figsize=(10, 8))
+        raster_plot = ax.imshow(raster_crop[0], extent=shape_extent)
+        ax.set_title(plot_title)
+        ax.set_axis_off()
+        fig.colorbar(raster_plot)
+        plt.show()
+    return raster_crop
+
+
+def reclassify_raster(input_raster_file, class_dict, outfile_path):
+    """
+    Reclassify raster data based on given class dictionary
+    :param input_raster_file: Input raster file path
+    :param class_dict: Classification dictionary containing (from, to) as keys and "becomes" as value
+    :param outfile_path: Output file path (only tiff file)
+    :return: Reclassified raster
+    """
+
+    raster_file = rio.open(input_raster_file)
+    raster_data = raster_file.read(1)
+    for key in class_dict.keys():
+        raster_data[np.logical_and(raster_data >= key[0], raster_data <= key[1])] = class_dict[key]
+    write_raster(raster_data, raster_file, outfile_path)
+    return raster_data
+
